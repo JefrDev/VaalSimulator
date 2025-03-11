868d7ce042bf0a4388cedb038585970ff1f53630
JeroenFredriksz <you@example.com>
2025-03-11 11:07:12 +0100

    rendering prefixes and suffixes now

--- vaal_simulator/src/components/ItemDisplay.tsx
+++ vaal_simulator/src/components/ItemDisplay.tsx
@@ -10,6 +10,9 @@ async function loadDefaultItem(itemName: string) {
     setItem(item);
 }
 
+function getFulltext(text: string, value: number) {
+    return text.replace('CURRENT', value.toString());
+}
 
   useEffect(() => {
     loadDefaultItem("Expert Feathered Sandals");
@@ -19,6 +22,24 @@ async function loadDefaultItem(itemName: string) {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Item Display</h1>
       <h2>{item?.itemName}</h2>
+      <div>
+        {item?.prefixes.map((prefix, index) => (
+          <div key={`prefix-${index}`}>
+            {Array.isArray(prefix.value) 
+              ? getFulltext(prefix.text, prefix.value[0]).replace('CURRENT', prefix.value[1].toString())
+              : getFulltext(prefix.text, prefix.value)
+            }
+          </div>
+        ))}
+        {item?.suffixes.map((suffix, index) => (
+          <div key={`suffix-${index}`}>
+            {Array.isArray(suffix.value)
+              ? getFulltext(suffix.text, suffix.value[0]).replace('CURRENT', suffix.value[1].toString()) 
+              : getFulltext(suffix.text, suffix.value)
+            }
+          </div>
+        ))}
+      </div>
       
     </div>
   );
