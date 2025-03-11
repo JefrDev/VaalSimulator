faf6d30aa9428407501cb808989512005cd96636
JeroenFredriksz <you@example.com>
2025-03-11 10:27:36 +0100

    fetching the data from the example json file

--- vaal_simulator/src/components/ItemDisplay.tsx
+++ vaal_simulator/src/components/ItemDisplay.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Item } from "../types/Item";
 
 
 export const ItemDisplay = () => {
   const [items, setItems] = useState<Item[]>([]);
+
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch('/docs/itemformat.json');
+      const data = await response.json();
+      setItems(data);
+    };
+    fetchItems();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
