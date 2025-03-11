7af8a81dddf9a748e78faa81b8babdb7fccd4593
JeroenFredriksz <you@example.com>
2025-03-11 10:39:38 +0100

    cleaner way to create a default item

--- vaal_simulator/src/components/ItemDisplay.tsx
+++ vaal_simulator/src/components/ItemDisplay.tsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
-import { Item } from "../types/Item";
+import { Item, createDefaultItem } from "../types/Item";
 
 
 export const ItemDisplay = () => {
-  const [items, setItems] = useState<Item[]>([]);
+  const [item, setItem] = useState<Item>();
 
+
+  const loadDefaultItem = async () => {
+    const item = createDefaultItem("Expert Feathered Sandals");
+    setItem(item);
+};
 
   useEffect(() => {
-    const fetchItems = async () => {
-      const response = await fetch('/docs/itemformat.json');
-      const data = await response.json();
-      setItems(data);
-    };
-    fetchItems();
+    loadDefaultItem();
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Item Display</h1>
+      <h2>{item?.itemName}</h2>
+      
     </div>
   );
 };
